660e3c16644eba39a4f1e4cd64a3d5c7a99eb5d4
just2102 <thesixteensaltines@gmail.com>
2023-02-17 10:30:05 +0900

    setup post routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,9 +6,38 @@ import Post from "../models/Post.js"
 dotenv.config()
 
 const router = express.Router()
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+
 router.route('/')
-.post((req,res)=>{
-    res.send('posted something')
+.get(async(req,res)=>{
+    try {
+        const posts = await Post.find({});
+        res.status(200).json({success:true, data: posts})
+    } catch (err) {
+        res.status(500).json({success:false, message: err})
+    }
+
+})
+.post(async(req,res)=>{
+    try {
+        const {name,prompt,photo} = req.body
+        const photoURL = await cloudinary.uploader.upload(photo);
+    
+        const newPost = await Post.create({
+            name,
+            prompt,
+            photo:photoURL.url,
+        })
+        res.status(201).json({success: true, data:newPost})
+    } catch(err) {
+        res.status(500).json({success:false, message: err})
+    }
+
 })
 
 export default router
